8e5ef033d9f4e654e5f47c8cfa38460a895e56fb
unique211 <vishal.rkcet@gmail.com>
2019-12-28 12:16:06 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Routing\UrlGenerator;   //change by vishal
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+         if(config('app.debug')!=true) {
+    \URL::forceScheme('https');
+  }
     }
 }
